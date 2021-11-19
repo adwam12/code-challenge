0d395a419014dda8371d635ca12a76a6b348c6e3
adwam12 <72263888+adwam12@users.noreply.github.com>
2021-11-18 21:40:39 -0500

    removed commented out redundant code

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,32 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const data = require("../data/sample.json");
-// const searchController = require('../controller/searchController.js')
+
 const controller = require("../controller")
-
-/* GET home page. */
-// router.get("/", function(req, res, next) {
-// 	res.render("index", {
-// 		title: "Express"
-// 	});
-// });
-
-
-// router.get("/data", function(req, res, next) {
-// 	res.json(data);
-//   print(data)
-// })
 
 router.get("/data", controller.searchController);
 
-
-// router.route("/lopi")
-//   .get(controller.search)
-
 router.route("/loanSearch/:id")
   .get(controller.searchController)
-
-
 
 
 module.exports = router;
